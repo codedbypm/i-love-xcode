e57440e41069c88fcd9872d99b276f3ad65c75de
Paolo Moroni <paolo@codedby.pm>
2021-01-28 13:59:00 +0100

    feat: add xcode file templpates for MVVM

---
+++ file-templates/show-diff.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+ ksdiff ~/Library/Developer/Xcode/Templates/File\ Templates/ templates
---
+++ file-templates/templates/MVVM/MVVM.xctemplate/___FILEBASENAME___ViewModel.swift
@@ -0,0 +1,16 @@
+//  ___FILEHEADER___
+
+import Foundation
+
+class ___VARIABLE_productName:identifier___ViewModel {
+    
+    // MARK: - Inputs
+    
+    // MARK: - Outputs
+
+    // MARK: - Init
+
+    init() {
+
+    }
+}
